0b591239ba38e6046c9bbf1b644b5880505614cb
Pawel Jasinski <pawel.jasinski@gmail.com>
2018-02-05 03:18:38 +0100

    WIP: upload work most of the time

--- src/main/java/ch/aerodigital/espcon/App.java
+++ src/main/java/ch/aerodigital/espcon/App.java
@@ -1,6 +1,15 @@
 package ch.aerodigital.espcon;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +46,9 @@ public class App {
     public static SerialPort serialPort;
     public static Terminal terminal;
     public static boolean waitForMarker;
+    public Timer timer;
+
+    private final PortReader portReader;
 
     private static final String MARKER = "~~~END~~~";
 
@@ -95,16 +107,19 @@ public class App {
         }
     }
 
+    public App() {
+        this.fileReadBuffer = new byte[FILE_UPLOAD_PACKET_SIZE];
+        this.portReader = new PortReader();
+    }
+
     public void run() throws IOException {
         openPort();
-
         terminal = TerminalBuilder.builder().system(true).build();
-
         History history;
         history = new DefaultHistory();
         LineReader reader = LineReaderBuilder.builder().terminal(terminal).history(history).build();
         reader.setVariable(LineReader.HISTORY_FILE, ".cmd-history");
-        String prompt = "press ENTER to sync";
+        String prompt = "press ENTER to sync ... ";
         while (true) {
             String line;
             try {
@@ -127,10 +142,13 @@ public class App {
             } catch (UserInterruptException e) {
                 // Ignore
             } catch (EndOfFileException e) {
+                if (timer != null) {
+                    timer.cancel();
+                }
                 try {
                     serialPort.closePort();
                 } catch (SerialPortException ex) {
-
+                    System.out.println("troulbe closing serial port " + ex);
                 }
                 return;
             }
@@ -200,11 +218,79 @@ public class App {
         }
     }
 
+
+    private class PortFilesUploader implements SerialPortEventListener {
+        private String rx_data;
+
+        public PortFilesUploader() {
+            rx_data = "";
+        }
+
+        public void serialEvent(SerialPortEvent event) {
+            String data, crc_parsed;
+            int crc_end_marker;
+            if (event.isRXCHAR() && event.getEventValue() > 0) {
+                try {
+                    data = serialPort.readString(event.getEventValue());
+                    rx_data = rx_data + data;
+                } catch (SerialPortException ex) {
+                    System.out.println("exception when receiving data:" + ex);
+                }
+                if (rx_data.contains("> ")) {
+                    rx_data = "";
+                }
+                crc_end_marker = rx_data.indexOf("~~~CRC-END~~~");
+                if (-1 != crc_end_marker) {
+                    int start = rx_data.indexOf("~~~CRC-START~~~");
+                    crc_parsed = rx_data.substring(start + 15, crc_end_marker);
+                    rx_data = rx_data.substring(crc_end_marker + 13);
+                    int crc_received = Integer.parseInt(crc_parsed);
+                    int crc_expected = CRC(currentChunk);
+                    if (crc_expected == crc_received) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print("E");
+                    }
+                }
+                if (rx_data.contains("~~~END~~~")) {
+                    try {
+                        System.out.println();
+                        promptQueue.put("> ");
+                    } catch (InterruptedException ex) {
+                        System.out.println("intrerrupted when giving prompt " + ex);
+                    }
+                    try {
+                        serialPort.removeEventListener();
+                        serialPort.addEventListener(portReader, SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS);
+                    } catch (SerialPortException ex) {
+                        System.out.println("Unable to restore command line processor");
+                    }
+                }
+            } else if (event.isCTS()) {
+                System.out.println("cts event");
+            } else if (event.isERR()) {
+                System.out.println("FileManager: Unknown serial port error received.");
+            } else {
+                System.out.println("wtf event" + event);
+            }
+        }
+    }
+
+    private int CRC(byte[] s) {
+        int cs = 0;
+        int x;
+        for (int i = 0; i < s.length; i++) {
+            x = s[i] & 0xFF;
+            cs = cs + (x * 20) % 19;
+        }
+        return cs;
+    }
+
     private final BlockingQueue promptQueue = new ArrayBlockingQueue(10);
 
     private String waitForPrompt() {
         try {
-            return (String) promptQueue.poll(1500, TimeUnit.MILLISECONDS);
+            return (String) promptQueue.poll(2000, TimeUnit.MILLISECONDS); // TODO: This timeout should be extendable
         } catch (InterruptedException ex) {
             System.out.println("interrupted " + ex);
         }
@@ -252,12 +338,12 @@ public class App {
                         + "  local k,v,l\n"
                         + "  for k,v in pairs(file.list()) do\n"
                         + "    l=string.format(\"%-20s\",k)\n"
-                        + "    print(l..\" : \"..v)\n"
+                        + "    print(l..' : '..v)\n"
                         + "  end\n"
                         + "end\n"
                         + "_dir()\n"
                         + "_dir=nil\n"
-                        + "print(\"" + MARKER + "\")\n";
+                        + "print('~~~'..'END'..'~~~')\n";
                 waitForMarker = true;
                 serialPort.writeString(cmd);
 
@@ -270,16 +356,147 @@ public class App {
                 throw new InvalidCommandException("expect exactly one parameter to --cat");
             }
             ViewFile(args[1]);
+        } else if (command.startsWith("--upload")) {
+            String args[] = command.split("\\s+");
+            if (args.length == 1 || args.length > 3) {
+                throw new InvalidCommandException("upload needs 1 or 2 arguments");
+            }
+            if (args.length == 2) {
+                uploadFile(args[1]);
+            } else {
+                uploadFile(args[1], args[2]);
+            }
         } else {
             throw new InvalidCommandException("not a valid command");
         }
     }
 
+    private void uploadFile(String src) {
+        uploadFile(src, src);
+    }
+
+    private static final int FILE_UPLOAD_PACKET_SIZE = 250;
+
+    private static int sendIndex = 0;
+
+    private ArrayList<String> sendBuf;
+
+    private InputStream srcFileIs;
+
+    private final byte[] fileReadBuffer;
+
+    private byte[] currentChunk;
+
+    private void uploadFile(String src, String target) {
+
+        File srcFile = new File(src);
+
+        long nFullPackets = srcFile.length() / FILE_UPLOAD_PACKET_SIZE;
+        long lastPacketSize = srcFile.length() % FILE_UPLOAD_PACKET_SIZE;
+        final long totalPackets = nFullPackets + (lastPacketSize != 0 ? 1 : 0);
+        long regularPacketSize = totalPackets == 1 ? lastPacketSize : FILE_UPLOAD_PACKET_SIZE;
+
+        String cmd = ""
+                + "_up=function(n,l,ll)\n"
+                + "  local cs=0\n"
+                + "  local i=0\n"
+                + "  local f\n"
+                + "  print('>'..' ')\n"
+                + "  uart.on('data', l, function(b)\n"
+                + "    i=i+1\n"
+                + "    f=file.open('" + target + "','a+')\n"
+                + "    f:write(b)\n"
+                + "    f:close()\n"
+                + "    cs=0\n"
+                + "    for j=1,l do\n"
+                + "      cs=cs+(b:byte(j)*20)%19\n"
+                + "    end\n"
+                + "    uart.write(0,'~~~CRC-'..'START~~~'..cs..'~~~CRC-'..'END~~~')\n"
+                + "    if i==n then\n"
+                + "      uart.on('data')\n"
+                + "      print('~~~'..'END'..'~~~')\n"
+                + "    end\n"
+                + "    if i==n-1 and ll>0 then\n"
+                + "      _up(1,ll,ll)\n"
+                + "    end\n"
+                + "  end,0)\n"
+                + "end\n"
+                + "file.remove('" + target + "')\n";
+        sendBuf = cmdPrep(cmd);
+        sendBuf.add("_up(" + totalPackets + "," + regularPacketSize + "," + lastPacketSize + ")");
+        try {
+            serialPort.removeEventListener();
+        } catch (SerialPortException ex) {
+            System.out.println("Uploader: Unable to deactivate serial event listener " + ex);
+        }
+        try {
+            serialPort.addEventListener(new PortFilesUploader(), SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS);
+        } catch (SerialPortException ex) {
+            System.out.println("Uploader: Add EventListener Error. Canceled. " + ex);
+            return;
+        }
+        try {
+            srcFileIs = new BufferedInputStream(new FileInputStream(srcFile));
+        } catch (FileNotFoundException ex) {
+            System.out.println("unable to open src");
+            return;
+        }
+        TimerTask taskPerformer = new TimerTask() {
+            public void run() {
+                if (sendIndex < sendBuf.size()) {
+                    try {
+                        serialPort.writeString(sendBuf.get(sendIndex) + "\r");
+                    } catch (SerialPortException ex) {
+                        System.out.println("failed to send file upload program " + ex);
+                    }
+                } else if ((sendIndex - sendBuf.size()) < totalPackets) {
+                    try {
+                        int size = srcFileIs.read(fileReadBuffer);
+                        if (size != fileReadBuffer.length) {
+                            currentChunk = Arrays.copyOfRange(fileReadBuffer, 0, size);
+                        } else {
+                            currentChunk = fileReadBuffer;
+                        }
+                        serialPort.writeBytes(currentChunk);
+                    } catch (IOException ex) {
+                        System.out.println("failed to read file chunk" + ex);
+                    } catch (SerialPortException ex) {
+                        System.out.println("failed to send file file chunk " + ex);
+                    }
+
+                } else {
+                    timer.cancel();
+                }
+                sendIndex++;
+            }
+        };
+        sendIndex = 0;
+        timer = new Timer();
+        timer.schedule(taskPerformer, 10, 300);
+    }
+
+    private ArrayList<String> cmdPrep(String cmd) {
+        ArrayList<String> s256 = new ArrayList<String>();
+        int i = 0;
+        s256.add("");
+        for (String subs : cmd.split("\n")) {
+            if ((s256.get(i).length() + subs.trim().length()) <= 250) {
+                s256.set(i, s256.get(i) + " " + subs.trim());
+            } else {
+                s256.set(i, s256.get(i) + "\r");
+                s256.add(subs);
+                i++;
+            }
+        }
+        return s256;
+    }
+
+
     private void ViewFile(String filename) {
         String cmd = ""
                 + "_view=function()\n"
                 + "  local _line, _f\n"
-                + "  _f=file.open(\"" + filename + "\",\"r\")\n"
+                + "  _f=file.open('" + filename + "','r')\n"
                 + "  repeat _line = _f:readline()\n"
                 + "    if (_line ~= nil) then\n"
                 + "      print(_line:sub(1,-2))\n"
@@ -289,7 +506,7 @@ public class App {
                 + "end\n"
                 + "_view()\n"
                 + "_view=nil\n"
-                + "print(\"" + MARKER + "\")\n";
+                + "print('~~~'..'END'..'~~~')\n";
         waitForMarker = true;
         try {
             serialPort.writeString(cmd);
@@ -301,11 +518,10 @@ public class App {
 
     public boolean openPort() {
         int nSpeed = baud;
-        boolean success = false;
         if (portOpen) {
             try {
                 serialPort.closePort();
-            } catch (Exception ex) {
+            } catch (SerialPortException ex) {
                 System.out.println("exception when closing serial port: " + ex);
             }
         } else {
@@ -314,18 +530,12 @@ public class App {
         serialPort = new SerialPort(serialPortDevice);
         portOpen = false;
         try {
-            success = serialPort.openPort();
-            if (!success) {
-                System.out.println("ERROR opening serial port " + serialPortDevice);
-                return success;
-            }
+            serialPort.openPort();
             SetSerialPortParams();
-            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS);
+            serialPort.addEventListener(portReader, SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS);
         } catch (SerialPortException ex) {
             System.out.println("failed to open port " + ex);
-            success = false;
         }
-        portOpen = success;
         if (portOpen) {
             System.out.println("Open port " + serialPortDevice + " - Success.");
         }
@@ -341,8 +551,8 @@ public class App {
                     SerialPort.PARITY_NONE,
                     false,
                     false);
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        } catch (SerialPortException ex) {
+            System.out.println("unable to configure serial port " + ex);
         }
         if (!success) {
             System.out.println("ERROR setting port " + serialPortDevice + " parameters.");
